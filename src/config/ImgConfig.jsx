2beceb29574e0a7b6b7c9a5b76d1b391a1ff9cfa
Danq Nhat Huy <94223877+dnh273@users.noreply.github.com>
2023-02-07 16:02:21 +0700

    Create ImgConfig.jsx

---
+++ src/config/ImgConfig.jsx
@@ -0,0 +1,11 @@
+import fileDefault from "../assets/images/file-blank-solid-240.png";
+import fileCSS from "../assets/images/file-css-solid-240.png";
+import filePDF from "../assets/images/file-pdf-solid-240.png";
+import filePNG from "../assets/images/file-png-solid-240.png";
+
+export const ImgConfig = {
+  default: fileDefault,
+  pdf: filePDF,
+  png: filePNG,
+  css: fileCSS,
+};
